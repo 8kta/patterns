702a7b8b33fc280b004683437abad4c675a7b63f
Alonso García <alonsog.gascon@gmail.com>
2022-04-05 16:22:23 -0500

    prototype

---
+++ src/main/java/Prototype/shapeWithCloneable/Circle.java
@@ -0,0 +1,27 @@
+package Prototype.shapeWithCloneable;
+
+
+public class Circle extends Shape  {
+    public int radius;
+
+    public Circle() {
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Circle shape2) || !super.equals(object2)) return false;
+        return shape2.radius == radius;
+    }
+}
