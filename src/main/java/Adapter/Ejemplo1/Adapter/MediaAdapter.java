95da11b0f6d0eeb7fd67556827935d23c007eb63
Alonso García <alonsog.gascon@gmail.com>
2022-04-07 22:14:00 -0500

    quitando VLC

--- src/main/java/Adapter/Ejemplo1/Adapter/MediaAdapter.java
+++ src/main/java/Adapter/Ejemplo1/Adapter/MediaAdapter.java
@@ -1,7 +1,6 @@
 package Adapter.Ejemplo1.Adapter;
 
 import Adapter.Ejemplo1.AdvancedMediaPlayer.AdvancedMusicPlayer;
-import Adapter.Ejemplo1.AdvancedMediaPlayer.VlcPlayer;
 import Adapter.Ejemplo1.AdvancedMediaPlayer.iTunesPlayer;
 import Adapter.Ejemplo1.MediaPlayer.MediaPlayer;
 
@@ -11,15 +10,9 @@ public class MediaAdapter implements MediaPlayer {
     AdvancedMusicPlayer advancedMusicPlayer;
 
     public MediaAdapter(String audioType) {
-
-        if (audioType.equalsIgnoreCase("WAV")) {
-            //SpecificRequest (Object)
-            advancedMusicPlayer = new iTunesPlayer();
-
-        } else if (audioType.equalsIgnoreCase("Mp4")) {
-            advancedMusicPlayer = new VlcPlayer();
-        }
+        advancedMusicPlayer = new iTunesPlayer();
     }
+
 
     @Override
     public void play(String audioType, String fileName) {
--- src/main/java/Adapter/Ejemplo1/MediaPlayer/AudioPlayer.java
+++ src/main/java/Adapter/Ejemplo1/MediaPlayer/AudioPlayer.java
@@ -5,22 +5,9 @@ import Adapter.Ejemplo1.Adapter.MediaAdapter;
 public class AudioPlayer implements MediaPlayer {
     MediaAdapter mediaAdapter;
 
-    @Override
-    public void play(String Type, String fileName) {
-        if (isInEnum(Type, MusicFormats.class)){
-            System.out.println("Playing audio file. Name: " + fileName);
-            mediaAdapter = new MediaAdapter(Type);
-        }
-        else if (isInEnum(Type, VideoFormats.class)){
-            System.out.println("Playing video file. Name: " + fileName);
-            mediaAdapter = new VideoAdapter(Type);
-        }
-
-
-    }
 
     @Override
-    public void play2(String audioType, String fileName) {
+    public void play(String audioType, String fileName) {
 
         //inbuilt support to play mp3 music files
         if (audioType.equalsIgnoreCase("Mp3")) {
